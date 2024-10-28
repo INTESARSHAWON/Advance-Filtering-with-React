e958316595258c7860aea76a68d63f220b8dd911
INTESARSHAWON <intesarshawon@gmail.com>
2024-10-28 09:38:49 +0600

    sidebar section

---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,23 @@
+import { FaShoppingCart } from "react-icons/fa";
+import "./Sidebar.css";
+import Category from "./Category/Category";
+import Price from "./Price/Price";
+import Colors from "./Colors/Colors";
+
+function Sidebar() {
+  return (
+    <>
+        <section className="sidebar">
+            <div className="logo-container">
+                <h1><FaShoppingCart /></h1>
+            </div>
+
+            <Category/>
+            <Price/>
+            <Colors/>
+        </section>
+    </>
+  )
+}
+
+export default Sidebar;
