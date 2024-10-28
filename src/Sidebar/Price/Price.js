951767ffc67b58a43fdba53b4ec2ab4c95c30fc0
INTESARSHAWON <intesarshawon@gmail.com>
2024-10-28 21:32:12 +0600

    price section

--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -2,8 +2,28 @@ import "./Price.css";
 
 function Price() {
   return (
-    <div>
-      
+    <div className="ml">
+      <h2 className="sidebar-title price-title">Price</h2>
+      <label className="sidebar-label-container">
+            <input type="radio" name="test2"/>
+            <span className="checkmark"></span>All
+      </label>
+      <label className="sidebar-label-container">
+            <input type="radio" name="test2"/>
+            <span className="checkmark"></span> $1-$50
+      </label>
+      <label className="sidebar-label-container">
+            <input type="radio" name="test2"/>
+            <span className="checkmark"></span> $51-$100
+      </label>
+      <label className="sidebar-label-container">
+            <input type="radio" name="test2"/>
+            <span className="checkmark"></span> $101-$150
+      </label>
+      <label className="sidebar-label-container">
+            <input type="radio" name="test2"/>
+            <span className="checkmark"></span> over $150
+      </label>
     </div>
   )
 }
