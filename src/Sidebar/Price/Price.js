8daab4047b0e3ecff3c388f954d33b4c9b102f5a
INTESARSHAWON <intesarshawon@gmail.com>
2024-10-29 20:46:39 +0600

    Refactor to clean code

--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -1,29 +1,15 @@
+import Input from "../../components/Input";
 import "./Price.css";
 
 function Price() {
   return (
     <div className="ml">
       <h2 className="sidebar-title price-title">Price</h2>
-      <label className="sidebar-label-container">
-            <input type="radio" name="test2"/>
-            <span className="checkmark"></span>All
-      </label>
-      <label className="sidebar-label-container">
-            <input type="radio" name="test2"/>
-            <span className="checkmark"></span> $1-$50
-      </label>
-      <label className="sidebar-label-container">
-            <input type="radio" name="test2"/>
-            <span className="checkmark"></span> $51-$100
-      </label>
-      <label className="sidebar-label-container">
-            <input type="radio" name="test2"/>
-            <span className="checkmark"></span> $101-$150
-      </label>
-      <label className="sidebar-label-container">
-            <input type="radio" name="test2"/>
-            <span className="checkmark"></span> over $150
-      </label>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
     </div>
   )
 }
